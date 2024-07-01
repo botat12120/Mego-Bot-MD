64f926c05af05fd19fa55356c59689f61801d5d6
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-07-01 17:54:53 +0300

    Update menu-الشرح.js

--- plugins/menu-الشرح.js
+++ plugins/menu-الشرح.js
@@ -1,9 +1,39 @@
-let handler = async (m, { conn }) => {
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-  let videoUrl = 'https://telegra.ph/file/9a39f321f2f250c1985f3.mp4'
-  let { name } = global.db.data.users[who]
-  m.react('📜')
-let str = `
+import { createHash } from 'crypto'
+   import PhoneNumber from 'awesome-phonenumber'
+   import { canLevelUp, xpRange } from '../lib/levelling.js'
+   import fetch from 'node-fetch'
+   import fs from 'fs'
+   const { levelling } = '../lib/levelling.js'
+   import moment from 'moment-timezone'
+   import { promises } from 'fs'
+   import { join } from 'path'
+   const time = moment.tz('Egypt').format('HH')
+   let wib = moment.tz('Egypt').format('HH:mm:ss')
+   //import db from '../lib/database.js'
+
+   let handler = async (m, { conn, usedPrefix, command}) => {
+       let d = new Date(new Date + 3600000)
+       let locale = 'en'
+       let week = d.toLocaleDateString(locale, { weekday: 'long' })
+       let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+       let _uptime = process.uptime() * 1000
+       let uptime = clockString(_uptime)
+   let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+   if (!(who in global.db.data.users)) throw `✳️ لم يتم العثور على المستخدم في قاعدة البيانات الخاصة بي`
+   let videoUrl = 'https://telegra.ph/file/09e09b9e0376dcff65ccc.mp4'
+   let user = global.db.data.users[who]
+   let { name, exp, diamond, lastclaim, registered, regTime, age, level, role, warn } = global.db.data.users[who]
+   let { min, xp, max } = xpRange(user.level, global.multiplier)
+   let username = conn.getName(who)
+   let math = max - xp
+   let prem = global.prems.includes(who.split`@`[0])
+   let sn = createHash('md5').update(who).digest('hex')
+   let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+   let more = String.fromCharCode(8206)
+   m.react('📜')
+   let readMore = more.repeat(850) 
+   let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+   let str = `
 *⎔⋅• ┗╼╃✦⊰⟦﷽⟧⊱✦╄╾┛ •⋅⎔*
 *❘╼╼𓏪 قائـمـة 📜 الاوامـر 𓏪╾╾❘*
 ── • ◈ • ──
@@ -486,16 +516,37 @@ let str = `
 * ╰⋅ ⋅ ── ⋅ ⋅ ── ✩ ── ⋅ ⋅ ── ⋅ ⋅ ╯*
 
 *~.¸¸ ❝ 𝑮𝑶𝑲𝑼 𝑩𝑶𝑻❝ ¸¸.~* 
-`
-  conn.sendMessage(m.chat, {
+   `.trim()
+       conn.sendMessage(m.chat, {
            video: { url: videoUrl }, caption: str,
      mentions: [m.sender,global.conn.user.jid],
      gifPlayback: true,gifAttribution: 0
        }, { quoted: m });
    };
+   handler.help = ['main']
+   handler.command = ['الشرح'] 
 
-handler.help = ['main']
-handler.tags = ['group']
-handler.command = ['الشرح']
+   export default handler
+   function clockString(ms) {
+       let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+       let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+       let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+       return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
 
-export default handler
+       function ucapan() {
+         const time = moment.tz('Egypt').format('HH')
+         let res = "بداية يوم سعيده ☀️"
+         if (time >= 4) {
+           res = "صباح الخير 🌄"
+         }
+         if (time >= 10) {
+           res = "مساء الخير ☀️"
+         }
+         if (time >= 15) {
+           res = "مساء الخير 🌇"
+         }
+         if (time >= 18) {
+           res = "مساء الخير 🌙"
+         }
+         return res
+       }
