d76b79e90a0d20e98cc6f15c29e63b74cb38d88e
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-07-02 21:55:26 +0300

    Update جوجل-درايف.js

--- plugins/جوجل-درايف.js
+++ plugins/جوجل-درايف.js
@@ -21,7 +21,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 }
 handler.help = ['gdrive']
 handler.tags = ['downloader', 'premium']
-handler.command = ['gdrive|جوجل-درايف']
+handler.command = ['gdrive', 'جوجل-درايف']
 handler.credit = true
 handler.premium = true
 
