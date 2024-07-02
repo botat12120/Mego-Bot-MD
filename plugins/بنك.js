c4d5d96b8012ff4b20dfde4b65a74346f2db6672
Amrobraih <154635591+Amrobraih@users.noreply.github.com>
2024-07-02 22:36:47 +0300

    Update بنك.js

--- plugins/بنك.js
+++ plugins/بنك.js
@@ -1,19 +1,38 @@
-let handler = async (m, { conn }) => {
-    let user = global.db.data.users[m.sender];
-    let name = conn.getName(m.sender);
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `*البــنــك*`;
-    await conn.sendMessage(m.chat, {
-   react: {
- text: "⛪",
- key: m.key,
-   }
-  })
+let handler = async (m, {conn, usedPrefix}) => {
 
-    conn.sendFile(m.chat, 'https://docs.google.com/spreadsheets/d/11arubNQ1HzoATfSjbhOJ877V7yURZKoHZ1edh1SIHSA/edit?usp=drivesdk', 'STORE.jpg' , message, m);
-};
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    let user = global.db.data.users[who]
+    let username = conn.getName(who)
+    //let { wealth } = global.db.data.users[who]
+    if (!(who in global.db.data.users)) throw `✳️ المستخدم غير موجود في قاعدة البيانات`
 
-handler.customPrefix = /^(بنك)$/i;
-handler.command = new RegExp;
+    var wealth = 'مفلس 😭'
+     if (`${user.bank}`           <= 3000){
+            wealth = 'مفلس 😭'
+      } else if (`${user.bank}`   <= 6000){
+            wealth = 'فقير 😢'
+        } else if (`${user.bank}` <= 100000){
+            wealth = 'متوسط 💸'
+        } else if (`${user.bank}` <= 1000000){
+            wealth = 'غني 💸💰'
+        } else if (`${user.bank}` <= 10000000){
+            wealth = 'مليونير 🤑'
+        } else if (`${user.bank}` <= 1000000000){
+            wealth = 'ملايين كتير فشخ 🤑'
+        } else if (`${user.bank}` <= 10000000000){
+            wealth = 'ملياردير 🤑🤑'
+        }    
 
-export default handler;
+    conn.reply(m.chat, `🏦 *بنك | ${username}*
+
+*🪙 العملات* : ${user.bank}
+
+*الثروة:* ${wealth}
+
+`, m, { mentions: [who] })  //${user.chicken}
+}
+handler.help = ['bank']
+handler.tags = ['economy']
+handler.command = ['البنك2', 'vault', 'بنك'] 
+
+export default handler
